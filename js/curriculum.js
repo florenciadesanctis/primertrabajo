b9ba322e9ed696805b0a466c3560ac3f372eacf3
florenciadesanctis <florenciadesanctis@gmail.com>
2023-01-30 17:17:05 -0300

    Trabajo Terminado
    Trabajo Terminado

---
+++ js/curriculum.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('mouseover',
+function(evt){
+    if
+    (evt.target.classList.contains('color')){
+        evt.target.style.color = 'orange';
+    }
+    });
+
+    window.addEventListener('mouseout',
+function(evt){
+    if
+    (evt.target.classList.contains('color')){
+        evt.target.style.color = 'black';
+        }
+    });
+
+
+    
+
